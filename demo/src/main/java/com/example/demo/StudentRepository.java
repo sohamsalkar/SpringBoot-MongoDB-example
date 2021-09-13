06d5e2f8b1b68aab027c0bf38122714584ae945f
Soham Salkar <63644015+sohamsalkar@users.noreply.github.com>
2021-09-13 23:01:38 +0530

    Delete demo directory

--- demo/src/main/java/com/example/demo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentRepository extends MongoRepository<Student, String>
-{
-	
-
-}
